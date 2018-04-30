483966f52ea7e836af9e58569835206164406661
Cyrille YAO <cyaoyapi@gmail.com>
2018-04-30 02:12:12 +0200

    P1C2 : Part 1 Chapter 2

---
+++ karadoc.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3.6 
+# coding:utf-8
+
+if __name__ == "__main__":
+	print("File run from shell!")
+else:
+	print("File imported as a module!")
---
+++ parity.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3.6
+# coding:utf-8
+
+def main():
+	""" Main function to start the program. """
+
+	pass
+if __name__ == "__main__":
+	main()
